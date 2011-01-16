9d344e06fb0dee072f77511c255079153c78db61
Fabio Yamate <fabioyamate@gmail.com>
2011-01-15 22:46:15 -0200

    Added command line code base.

---
+++ lib/lastfm/cli.rb
@@ -0,0 +1,51 @@
+require 'optparse'
+
+module Lastfm
+  class Cli
+    attr_reader :options
+
+    def initialize(argv)
+      @argv = argv
+      @options = {}
+      parse_options
+    end
+
+    def run
+      case @argv.last
+      when 'events'
+        'called events'
+      else
+        if @options[:help]
+          opt_parser
+        else
+          "lastfm: try 'lastfm --help' or 'lastfm -h' for more information"
+        end
+      end
+    end
+
+    private
+
+    def parse_options
+      opt_parser.parse!(@argv)
+    end
+
+    def opt_parser
+      @opt_parser ||= OptionParser.new do |opt|
+        opt.banner = 'Usage: lastfm [options] <command>'
+        opt.separator ''
+        opt.separator 'Commands'
+        opt.separator '    events   # retrieve events from a specific location'
+        opt.separator ''
+        opt.separator 'Options'
+
+        opt.on('-l', '--location LOCATION', '# location name') do |location|
+          @options[:location] = location
+        end
+
+        opt.on('-h', '--help', '# command line help') do |help|
+          @options[:help] = help
+        end
+      end
+    end
+  end
+end
--- test/simple_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TestSimple < MiniTest::Unit::TestCase
-  def test_simple
-    assert true
-  end
-end
